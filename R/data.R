5f616328e85d24a9e43da0adf03d922db8134c7e
javad vahdat <vahdatjavad@gmail.com>
2025-03-27 15:15:04 +1100

    fixed some issues

--- R/data.R
+++ R/data.R
@@ -1,101 +1,101 @@
-#' cuttlefish
-#'
+#' Cuttlefish Sightings Dataset
+#' 
 #' Sightings of cuttlefish downloaded from https://www.ala.org.au
 #'
 #' \itemize{
-#'   \item id, record id given by Atlas
-#'   \item name, scientific name
-#'   \item lon, lat spatial coordinates of the sighting
-#'   \item date, year, month, wday, time of sighting
-#'   \item source, original provider of the record
-#'   \item stnid, station id for the nearest weather station
+#'   \item id: Record ID given by Atlas
+#'   \item name: Scientific name
+#'   \item lon, lat: Spatial coordinates of the sighting
+#'   \item date, year, month, wday, time: Date and time of sighting
+#'   \item source: Original provider of the record
+#'   \item stnid: Station ID for the nearest weather station
 #' }
 #'
-#' @name cuttlefish
-#' @docType data
-#' @format A 750 x 9 numeric array
+#' @format A data frame with 750 rows and 9 variables
+#' @source https://www.ala.org.au
 #' @keywords datasets
+#' @rdname cuttlefish
 #' @examples
-#'
+#' data(cuttlefish)
 #' head(cuttlefish)
 "cuttlefish"
 
-#' weather
-#'
-#' Daily records of precipitation, min and max temperature, covering locations and times of sightings, as available using the `rnoaa` package.
+
+#' Weather Data from Sightings Locations
+#' 
+#' Daily records of precipitation, and minimum and maximum temperature, 
+#' covering locations and times of sightings, as available using the `rnoaa` package.
 #'
 #' \itemize{
-#'   \item stnid, station id
-#'   \item lon, lat, elev spatial coordinates of the station
-#'   \item date, day of records
-#'   \item prcp precipitation in mm
-#'   \item tmax, tmin temperature in Celsius
+#'   \item stnid: Station ID
+#'   \item lon, lat, elev: Spatial coordinates and elevation of the station
+#'   \item date: Date of the record
+#'   \item prcp: Precipitation in mm
+#'   \item tmax, tmin: Temperature in Celsius
 #' }
 #'
-#' @name weather 
-#' @docType data
-#' @format A 15274 x 9 numeric array
+#' @format A data frame with 15,274 rows and 9 variables
 #' @keywords datasets
+#' @rdname weather
 #' @examples
-#'
+#' data(weather)
 #' head(weather)
 "weather"
 
-#' tourism
+#' Quarterly Tourism Data
 #'
-#' Quarterly records of tourist counts in regions across the country
+#' Quarterly records of tourist counts in regions across Australia.
 #'
 #' \itemize{
-#'   \item Quarter
-#'   \item Region
-#'   \item Purpose
-#'   \item Trips
-#'   \item lon
-#'   \item lat
-#'   \item stnid
+#'   \item Quarter: The quarter in which the tourism activity was recorded (e.g., 2019 Q1)
+#'   \item Region: Geographic region where the data was collected
+#'   \item Purpose: Purpose of the trip (e.g., Holiday, Business)
+#'   \item Trips: Number of trips
+#'   \item lon, lat: Coordinates of the region
+#'   \item stnid: Station ID used for matching with weather data
 #' }
 #'
-#' @name tourism
-#' @docType data
-#' @format A 1228000 x 4 numeric array
+#' @format A data frame with 1,228,000 rows and 7 variables
 #' @keywords datasets
+#' @rdname tourism
 #' @examples
-#'
-#' library(tsibble)
 #' data(tourism)
-#' tourism <- tourism |>
-#'   mutate(Quarter = yearquarter(myd(Quarter))) |>
-#'   as_tsibble(key = c(Region, Purpose), index = Quarter)
+#' head(tourism)
 #'
+#' # Example of converting to tsibble (requires tsibble package)
+#' # library(tsibble)
+#' # tourism <- tourism |>
+#' #   dplyr::mutate(Quarter = yearquarter(lubridate::myd(Quarter))) |>
+#' #   as_tsibble(key = c(Region, Purpose), index = Quarter)
 "tourism"
 
-#' oz_lga
+
+#' Local Government Areas (LGA) Polygons for Australia
 #'
-#' LGA polygons for Australia
+#' A spatial polygons object representing the boundaries of local government areas (LGAs) in Australia.
 #'
-#' @name oz_lga
-#' @docType data
-#' @format A spatial polygon object 
+#' @format A spatial polygons object 
 #' @keywords datasets
+#' @rdname oz_lga
 #' @examples
-#'
+#' data(oz_lga)
 #' head(oz_lga)
 "oz_lga"
 
-#' stations
+
+#' Nearest Weather Stations for Cuttlefish Records
 #'
-#' This is the nearest stations for each records in cuttlefish data
+#' A dataset of the nearest weather stations corresponding to each record in the cuttlefish sightings data.
 #'
 #' \itemize{
-#'   \item stnid, record id given by Atlas
-#'   \item lon, lat spatial coordinates of the sighting
+#'   \item stnid: Station ID
+#'   \item lon, lat: Spatial coordinates of the station
 #' }
 #'
-#' @name stations
-#' @docType data
-#' @format A 750 x 3 numeric array
+#' @format A data frame with 750 rows and 3 variables
 #' @keywords datasets
+#' @rdname stations
 #' @examples
-#'
+#' data(stations)
 #' head(stations)
 "stations"
